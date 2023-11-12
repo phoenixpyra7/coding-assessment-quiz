15627291c59830f66d7d9479058f711b8a0bba8f
Seven <phoenixpyra7@gmail.com>
2023-11-12 15:25:05 -0600

    added event listeners

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,6 +1,7 @@
 var startScreen = document.querySelector('.start');
 var questionsScreen = document.querySelector('.questions');
 var scoreScreen = document.querySelector('.score');
+var startButton = document.querySelector('#startbtn');
 
 
 function showStart () {
@@ -9,20 +10,33 @@ function showStart () {
     scoreScreen.style.display = "none";
 }
 
-function showQuiz () {
+function showQuestions () {
     startScreen.style.display = "none";
     questionsScreen.style.display = null;
     scoreScreen.style.display = "none";
 }
 
-function showEnd () {
+function showScore () {
     startScreen.style.display = "none";
-    questionsScreen.style.display = null;
-    scoreScreen.style.display = "none";
+    questionsScreen.style.display = "none";
+    scoreScreen.style.display = null;
 }
+
+//how would i know where to put eventlisteners? also single quote also?
+startButton.addEventListener('click', function(event) { 
+    showQuestions();
+});
+
+//event listener to proceed to the score
+questionsScreen.addEventListener('click', function(event) {
+    if (event.target.matches('button')); //what is the word button referencing
+        //console.log('the button was hit')    -this was to test the button, which worked
+        showScore();
+});
+
 
 function init () {
-
+    showStart();
 }
 
 init(); 
