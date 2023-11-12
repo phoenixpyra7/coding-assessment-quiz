0de58118fb13e81e6628229af71b5798b574f754
Seven <phoenixpyra7@gmail.com>
2023-11-12 16:52:57 -0600

    added background color and button color.

--- assets/css/script.js
+++ assets/css/script.js
@@ -4,42 +4,42 @@ var scoreScreen = document.querySelector('.score');
 var startButton = document.querySelector('#startbtn');
 
 
-function showStart () {
+function showStart() {
     startScreen.style.display = null;
     questionsScreen.style.display = "none";
     scoreScreen.style.display = "none";
 }
 
-function showQuestions () {
+function showQuestions() {
     startScreen.style.display = "none";
     questionsScreen.style.display = null;
     scoreScreen.style.display = "none";
 }
 
-function showScore () {
+function showScore() {
     startScreen.style.display = "none";
     questionsScreen.style.display = "none";
     scoreScreen.style.display = null;
 }
 
 //how would i know where to put eventlisteners? also single quote also?
-startButton.addEventListener('click', function(event) { 
+startButton.addEventListener('click', function (event) {
     showQuestions();
 });
 
 //event listener to proceed to the score
-questionsScreen.addEventListener('click', function(event) {
+questionsScreen.addEventListener('click', function (event) {
     if (event.target.matches('button')); //what is the word button referencing
-        //console.log('the button was hit')    -this was to test the button, which worked
-        showScore();
+    //console.log('the button was hit')    -this was to test the button, which worked
+    showScore();
 });
 
 
-function init () {
+function init() {
     showStart();
 }
 
-init(); 
+init();
 
 
 
