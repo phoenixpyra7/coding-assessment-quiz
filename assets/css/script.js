7c8b8311b70a0c0ee68ad7cf173ba54547943595
Seven <phoenixpyra7@gmail.com>
2023-11-12 14:46:06 -0600

    added function show start, show questions, show score.

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,12 +1,26 @@
+var startScreen = document.querySelector('.start');
+var questionsScreen = document.querySelector('.questions');
+var scoreScreen = document.querySelector('.score');
+
+
 function showStart () {
-
+    startScreen.style.display = null;
+    questionsScreen.style.display = "none";
+    scoreScreen.style.display = "none";
 }
+
 function showQuiz () {
-
+    startScreen.style.display = "none";
+    questionsScreen.style.display = null;
+    scoreScreen.style.display = "none";
 }
+
 function showEnd () {
-
+    startScreen.style.display = "none";
+    questionsScreen.style.display = null;
+    scoreScreen.style.display = "none";
 }
+
 function init () {
 
 }
