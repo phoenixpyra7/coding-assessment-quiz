cd99f70499ed5f36d3053f2a7e922923ac8a4496
Seven <phoenixpyra7@gmail.com>
2023-11-12 19:00:49 -0600

    added quiz questions with answers to questions.js

---
+++ assets/javascript/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        question: 'Which HTML tag does not require a closing tag?',
+        options: ['section', 'div', 'br', 'body'],
+        correctAnswer: 2
+    },
+    {
+        question: "Which of the following data types is called a compound data type?",
+        options: ["String", "Array", "Boolean", "Symbol"],
+        correctAnswer: 2
+    },
+    {
+        question: "Which keyword declares a local variable, meaning the variable can only be accessed within the block of code where it is declared ?",
+        options: ["let", "const", "var"],
+        correctAnswer: 0
+    },
+    {
+        question: "How do you inset a copyright symbol in HTML?",
+        options: ["@copy", "&#167", "C/2023", "&#169"],
+        correctAnswer: 3
+    },
+    {
+        question: "Which of the following is not a positioning property in CSS?",
+        options: ["Fixed", "Straight", "Sticky", "Absolute"],
+        correctAnswer: 1
+    }
+];
