b738eec979e1bd47a96641c04e4c978ede3d2c4e
Seven <phoenixpyra7@gmail.com>
2023-11-13 12:35:57 -0600

    tried to connect questions, made more vans and functions

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,27 +1,27 @@
 var questions = [
     {
         question: 'Which HTML tag does not require a closing tag?',
-        options: ['section', 'div', 'br', 'body'],
+        options: ['A. section', 'B. div', 'C. br', 'D. body'],
         correctAnswer: 2
     },
     {
         question: "Which of the following data types is called a compound data type?",
-        options: ["String", "Array", "Boolean", "Symbol"],
+        options: ["A. String", "B. Array", "C. Boolean", "D. Symbol"],
         correctAnswer: 2
     },
     {
         question: "Which keyword declares a local variable, meaning the variable can only be accessed within the block of code where it is declared ?",
-        options: ["let", "const", "var"],
+        options: ["A. let", "B. const", "C. var"],
         correctAnswer: 0
     },
     {
         question: "How do you inset a copyright symbol in HTML?",
-        options: ["@copy", "&#167", "C/2023", "&#169"],
+        options: ["A. @copy", "B. &#167", "C. C/2023", "D. &#169"],
         correctAnswer: 3
     },
     {
         question: "Which of the following is not a positioning property in CSS?",
-        options: ["Fixed", "Straight", "Sticky", "Absolute"],
+        options: ["A. Fixed", "B. Straight", "C. Sticky", "D. Absolute"],
         correctAnswer: 1
     }
 ];
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,7 +2,9 @@ var startScreen = document.querySelector('.start');
 var questionsScreen = document.querySelector('.questions');
 var scoreScreen = document.querySelector('.score');
 var startButton = document.querySelector('#startbtn');
-var headerContainer =document.querySelector('.header'); //not sure if i need this
+var headerContainer = document.querySelector('.header'); //not sure if i need this
+var question = document.querySelector('question');
+var nextBtn = document.querySelector('next');
 
 
 function showStart() {
@@ -10,23 +12,34 @@ function showStart() {
     questionsScreen.style.display = "none";
     scoreScreen.style.display = "none";
 }
-
-function showQuestions() {
-    startScreen.style.display = "none";
-    questionsScreen.style.display = null;
-    scoreScreen.style.display = "none";
-}
-
-function showScore() {
-    startScreen.style.display = "none";
-    questionsScreen.style.display = "none";
-    scoreScreen.style.display = null;
-}
 //event listener to proceed to the questions
 startButton.addEventListener('click', function (event) {
     if (event.target.matches('button'));
     showQuestions();
 });
+
+
+
+
+
+function showQuestions() {
+    startScreen.style.display = "none";
+    questionsScreen.style.display = null;
+    scoreScreen.style.display = "none";
+    currentQuestionIndex = 0;
+}
+function setNextQuestion() {
+    showQuestion(currentQuestionIndex)
+}
+
+function showQuestion() {
+    question.innerHTML = "";
+
+
+}
+
+
+
 
 //event listener to proceed to the score
 questionsScreen.addEventListener('click', function (event) {
@@ -34,6 +47,15 @@ questionsScreen.addEventListener('click', function (event) {
     //console.log('the button was hit')    -this was to test the button, which worked
     showScore();
 });
+
+function showScore() {
+    startScreen.style.display = "none";
+    questionsScreen.style.display = "none";
+    scoreScreen.style.display = null;
+}
+
+
+
 
 
 function init() {
